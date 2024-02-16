9a370b8d0ebaae1c328161a0ab9f995bbf4b8a7e
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-16 12:29:46 +0900

    commit

--- config.lua
+++ config.lua
@@ -7,11 +7,20 @@
 -- optionとか
 
 -- keymap
+lvim.leader = "\\"
+
 lvim.keys.normal_mode["<C-n>"] = ":NvimTreeToggle<CR>"
 -- Tips: lunarvimのデフォルトは<gvらしい
 lvim.keys.visual_mode["<"] = false
 -- >も同様に
 lvim.keys.visual_mode[">"] = false
+
+-- telescopeの場合少し特殊で、、telescole.builtinをrequireして使う
+local builtin = require("telescope.builtin")
+lvim.keys.normal_mode["<leader>ff"] = builtin.find_files
+lvim.keys.normal_mode["<leader>fg"] = builtin.live_grep
+lvim.keys.normal_mode["<leader>fb"] = builtin.buffers
+lvim.keys.normal_mode["<leader>fh"] = builtin.help_tags
 
 -- override
 -- builtinで既存のプラグインの有効/無効、及びconfig内の調整ができる
@@ -20,16 +29,9 @@ lvim.builtin.bufferline.active = false
 lvim.builtin.which_key.active = false
 -- 実はLunarvimのデフォルトのテーマと全く同じなんだけど、設定例として記載しておく。
 lvim.colorscheme = "lunar"
-lvim.leader = "\\"
+
 -- フォーマット時自動でセーブするか
 lvim.format_on_save.enabled = true
-
--- telescopeの場合少し特殊で、、telescole.builtinをrequireして使う
-local builtin = require("telescope.builtin")
-lvim.keys.normal_mode["<leader>ff"] = builtin.find_files
-lvim.keys.normal_mode["<leader>fg"] = builtin.live_grep
-lvim.keys.normal_mode["<leader>fb"] = builtin.buffers
-lvim.keys.normal_mode["<leader>fh"] = builtin.help_tags
 
 -- lsp
 -- mason-lspconfigに登録されているもののみ選択可能
@@ -40,19 +42,19 @@ lvim.lsp.installer.setup.ensure_installed = {
 -- plugins
 -- 導入方法はlazy.nvimの標準的な方法と全く同じ
 lvim.plugins = {
-	{
+  {
     -- linterやformatterだけインストール自動化したい場合はこれがないと自動化するのが難しい
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = function()
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"stylua",
-				},
-			})
-		end,
-	},
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "stylua",
+        },
+      })
+    end,
+  },
 }
